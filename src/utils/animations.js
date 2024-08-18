aa801cc4312037873575a8941e46f24c1603d0c6
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-08-18 23:23:50 +0530

    fix: prettier

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,42 +1,44 @@
-import gsap from "gsap"
+import gsap from "gsap";
 
-import { ScrollTrigger } from "gsap/all"
+import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
 export const animateWithGsapTimeline = (
-    timeline,
-    rotationRef,
-    rotationState,
-    firstTarget,
-    secondTarget,
-    aniamtionProps,
+  timeline,
+  rotationRef,
+  rotationState,
+  firstTarget,
+  secondTarget,
+  aniamtionProps
 ) => {
-    if (!timeline) {
-        console.error("Timeline is undefined. Make sure you are passing a valid GSAP timeline.");
-        return;
-    }
-
-    timeline.to(rotationRef.current.rotation, {
-        y: rotationState,
-        duration: 1,
-        ease: "power2.inOut",
-    });
-
-    timeline.to(
-        firstTarget,
-        {
-            ...aniamtionProps,
-            ease: "power2.inOut",
-        },
-        "<"
+  if (!timeline) {
+    console.error(
+      "Timeline is undefined. Make sure you are passing a valid GSAP timeline."
     );
+    return;
+  }
 
-    timeline.to(
-        secondTarget,
-        {
-            ...aniamtionProps,
-            ease: "power2.inOut",
-        },
-        "<"
-    );
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...aniamtionProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+
+  timeline.to(
+    secondTarget,
+    {
+      ...aniamtionProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
 };
