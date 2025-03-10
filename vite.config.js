9556cd6888b50a57ec5d0ab6fe1bae771b78cc7d
ErandaMadusanka <emsamarasinghe@gmail.com>
2025-03-10 15:22:06 +0530

    fix: Tailwind CSS with webpack

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // tailwindcss(),
   plugins: [react()],
   build: {
-    outDir: "./dist",
+    outDir: './dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: "/index.html",
+      input: '/index.html',
     },
   },
 });
